55e67ce45456c498069c6209513b6ab6e4b0c7c0
Jamal Hansen <jamal.hansen@gmail.com>
2026-04-21 15:57:38 -0500

    latest updates

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -8,7 +8,9 @@ from yt_transcription_summarizer.logic import (
 
 
 def test_extract_video_id():
-    assert extract_video_id("https://www.youtube.com/watch?v=dQw4w9WgXcQ") == "dQw4w9WgXcQ"
+    assert (
+        extract_video_id("https://www.youtube.com/watch?v=dQw4w9WgXcQ") == "dQw4w9WgXcQ"
+    )
     assert extract_video_id("https://youtu.be/dQw4w9WgXcQ") == "dQw4w9WgXcQ"
 
 
